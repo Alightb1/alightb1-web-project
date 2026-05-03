7fc4abb54c157a0b8cf42a0f054cef547f90fc17
Anthony Lightbourn <alightb1@nmsu.edu>
2026-05-03 15:12:28 -0600

    converted previous links into buttons

---
+++ javascript/javascript.js
@@ -0,0 +1,25 @@
+
+
+
+
+
+document.getElementById("Segenbut").onclick = () => {
+    window.location.href = "../html/SengenParkTrail.html";
+};
+
+document.getElementById("PinBut").onclick = () => {
+    window.location.href = "../html/PineTree.html";
+};
+
+document.getElementById("Oberbut").onclick = () => {
+    window.location.href ="../html/Oberstdorf.html";
+};
+document.getElementById("Glacierbut").onclick = () => {
+    window.location.href =  "../html/Glacier.html";
+};
+
+document.getElementById("Gilabut").onclick = () => {
+    window.location.href = "../html/Gila.html";
+};
+
+
